c275847a2e6c9d7d7b5ee6364dadc3c6ca7a9d84
mayamohamed-16001727 <mayaelaraby2@gmail.com>
2025-11-11 09:56:40 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,48 +1,48 @@
-#include <iostream>
-#include <string>
 #include "Person.h"
+#include "Course.h"
 #include "Student.h"
 #include "Instructor.h"
-#include "Course.h"
-
-
+#include <string>
+#include <iostream>
 using namespace std;
+ void Person::intialize(string name, int id){
+    this->name=name;
+    this->id=id;
+}
+void Person:: display(){
+cout<<"name = "<< name<<endl;
+cout<<"id= "<< id << endl;
+}
+string Person::getName(){
+    return name;
+}
 
-// ==================== Person Class Implementation =========================
+void Student::display(){
+
+cout<<"yearlevel= "<< yearLevel<<endl;
+cout<<"major= "<<major<<endl;
+}
+void Instructor ::display(){
+    cout<<department<<endl;
+    cout<<experienceYears<<endl;
+}
+void Course:: addStudent(const Student& s){
+    Student* newstudent= new Student[currentStudents]
+    for(int i=0;i<currentstudents;i++){
+        students[i]=newstudent[i]
+    }
+    newstudent[currentStudents]=s;
+    delete students;
+}
+void Course::displayCourseInfo(){
+    cout<< CourseCode <<endl;
+    cout<< CourseName <<endl;
+    cout<<maxStudents<<endl;
+}
 
 
-
-
-
-
-
-
-// ==================== Student Class Implementation ====================
-
-
-
-
-
-
-// ==================== Instructor Class Implementation ====================
-
-
-
-
-
-
-// ==================== Course Class Implementation ====================
-
-
-
-
-
-
-
-
-// ==================== Main Function ====================
 int main() {
-   
+   Student s1;
     
     return 0;
 }
