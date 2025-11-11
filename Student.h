c7092285d441f990d37ab5d2354f2f5bb0abfc88
mayamohamed-16001727 <mayaelaraby2@gmail.com>
2025-11-11 09:54:15 +0200

    Student.h

--- Student.h
+++ Student.h
@@ -1,12 +1,16 @@
 #ifndef STUDENT_H
 #define STUDENT_H
-
 #include "Person.h"
-#include <string>
-#using namespace std;
-//#write student class here
+class Student{
+    private:
+    int yearLevel;
+    string major;
+    public:
+    
+    void display();
 
-
+};
+#endif  
 
 
 
